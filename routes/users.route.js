28e2d9a20f35ca7494a6f64fc5c9fdb1eee441e3
muhameddadel <muhameddadel2000@gmail.com>
2023-09-16 14:08:18 +0300

    modification

--- routes/users.route.js
+++ routes/users.route.js
@@ -5,28 +5,31 @@ const verifyToken = require("../middlewares/verifyToken");
 const multer  = require('multer');
 const appError = require("../utils/appError");
 
-const storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-                cb(null, 'uploads')
+const diskStorage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads');
         },
-        filename: (req, file, cb) => {
-                const extension = file.mimetype.split('/')[1]
-                const fileName = `user-${Date.now()}.${extension}`
-                cb(null, fileName)
-        },
-})
-
-const fileFilter = (req, file, cb) => {
-        if (file.mimetype.split('/')[1] === "image") {
-                return cb(null, tr)
-        }else {
-                return cb(appError.create("Invalid file format", 400), false)
+        filename: function(req, file, cb) {
+            const ext = file.mimetype.split('/')[1];
+            const fileName = `user-${Date.now()}.${ext}`;
+            cb(null, fileName);
         }
-}
-const upload = multer({
-        storage, 
+})
+    
+const fileFilter = (req, file, cb) => {
+        const imageType = file.mimetype.split('/')[0];
+        
+        if(imageType === 'image') {
+            return cb(null, true)
+        } else {
+            return cb(appError.create('file must be an image', 400), false)
+        }
+    }
+    
+const upload = multer({ 
+        storage: diskStorage,
         fileFilter
- })
+})
 
 const router = express.Router()
 
@@ -38,7 +41,6 @@ router.route('/register')
 
 router.route('/login')
         .post(userController.login)
-
 
 
 module.exports = router;
